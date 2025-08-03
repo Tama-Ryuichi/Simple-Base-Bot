fba7248d18c417c5b3a12cf9f7879479919bb847
T-Яyuichi <tamaryuichix@gmail.com>
2025-08-03 17:28:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -24,56 +24,67 @@ Don't Remove This Credits
 */
 
 console.clear();
+console.log("Loading...");
 require('./settings/config');
-
-const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    DisconnectReason,
-    fetchLatestBaileysVersion,
-    generateForwardMessageContent,
-    prepareWAMessageMedia,
-    generateWAMessageFromContent,
-    generateMessageID,
-    downloadContentFromMessage,
-    makeCacheableSignalKeyStore,
-    makeInMemoryStore,
-    jidDecode,
-    proto,
-    getAggregateVotesInPollMessage,
-    PHONENUMBER_MCC
-} = require("@whiskeysockets/baileys");
-
-const chalk = require('chalk');
-const pino = require('pino');
+process.on("uncaughtException", console.error);
+const fs = require("fs");
+const util = require("util");
+const pino = require("pino");
+const fetch = require("node-fetch");
 const { Boom } = require('@hapi/boom');
-const fs = require('fs');
-const FileType = require('file-type');
+const chalk = require("chalk");
 const readline = require("readline");
-const PhoneNumber = require('awesome-phonenumber');
-const path = require('path');
-const NodeCache = require("node-cache");
-const axios = require("axios")
+const {
+default: makeWASocket,
+makeCacheableSignalKeyStore,
+useMultiFileAuthState,
+DisconnectReason,
+fetchLatestBaileysVersion,
+generateForwardMessageContent,
+prepareWAMessageMedia,
+generateWAMessageFromContent,
+generateMessageID,
+downloadContentFromMessage,
+makeInMemoryStore,
+getContentType,
+jidDecode,
+MessageRetryMap,
+getAggregateVotesInPollMessage,
+proto,
+delay
+} = require("@whiskeysockets/baileys")
+const PhoneNumber = require("awesome-phonenumber");
+const FileType = require("file-type");
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, sleep } = require('./system/storage.js');
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid, addExif } = require('./system/exif.js');
+const { color } = require("./system/color.js");
 
-const usePairingCode = true; // true pairing / false QR
+const usePairingCode = true;
 
 const question = (text) => {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-    return new Promise((resolve) => {
-        rl.question(text, resolve);
-    });
+    return new Promise(resolve => rl.question(text, answer => {
+        rl.close();
+        resolve(answer);
+    }));
 };
 
-//===================
 async function connectToWhatsApp() {
     const { state, saveCreds } = await useMultiFileAuthState("./session");
+
+    const versionData = await fetch('https://raw.githubusercontent.com/Tama-Ryuichi/bails/refs/heads/master/lib/Defaults/baileys-version.json').then(res => res.json());
+
     const client = makeWASocket({
         printQRInTerminal: !usePairingCode,
+        auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "silent" }))
+        },
+        version: versionData.version,
+        browser: ["Ubuntu", "Chrome", "20.0.04"],
         syncFullHistory: true,
         markOnlineOnConnect: true,
         connectTimeoutMs: 60000,
@@ -81,12 +92,8 @@ async function connectToWhatsApp() {
         keepAliveIntervalMs: 10000,
         generateHighQualityLinkPreview: true,
         patchMessageBeforeSending: (message) => {
-            const requiresPatch = !!(
-                message.buttonsMessage ||
-                message.templateMessage ||
-                message.listMessage
-            );
-            if (requiresPatch) {
+            const needsPatch = !!(message.buttonsMessage || message.templateMessage || message.listMessage);
+            if (needsPatch) {
                 message = {
                     viewOnceMessage: {
                         message: {
@@ -99,79 +106,152 @@ async function connectToWhatsApp() {
                     },
                 };
             }
-
             return message;
         },
-        version: (await (await fetch('https://raw.githubusercontent.com/WhiskeySockets/Baileys/master/src/Defaults/baileys-version.json')).json()).version,
-        browser: ["Ubuntu", "Chrome", "20.0.04"],
-        logger: pino({
-            level: 'silent' // Set 'fatal' for production
-        }),
-        auth: {
-            creds: state.creds,
-            keys: makeCacheableSignalKeyStore(state.keys, pino().child({
-                level: 'silent',
-                stream: 'store'
-            })),
-        }
+        logger: pino({ level: "silent" }),
     });
 
     if (!client.authState.creds.registered) {
-        const phoneNumber = await question(console.log("Please Input Your Number Bot. Example : 62xxxxxxxxx"));
-        const code = await client.requestPairingCode(phoneNumber.trim());
-        console.log("This Your Pairing Code:");
-        console.log(`${code}`);
+        const phoneNumber = await question(chalk.blue.bold("Masukan Nomor WhatsApp : "));
+        try {
+            const code = await client.requestPairingCode(phoneNumber.trim());
+            console.log(chalk.blue.bold("Pairing Code: ") + chalk.white.bold(code));
+        } catch (err) {
+            console.log(chalk.redBright("Gagal mendapatkan pairing code!"));
+            console.error(err);
+            process.exit(1);
+        }
     }
 
     const store = makeInMemoryStore({
-        logger: pino().child({
-            level: 'silent',
-            stream: 'store'
-        })
+        logger: pino({ level: "silent" })
     });
 
     store.bind(client.ev);
+    
+    client.ev.on('connection.update', async (update) => {
+    let { stats } = require("./system/updates.js");
+    stats({ update, client, Boom, DisconnectReason, sleep, color, connectToWhatsApp });
+    })
 
-    //===================
-    client.ev.on('call', async (caller) => {
-        console.log("PANGGILAN MASUK");
+    client.ev.on("messages.upsert", async chatUpdate => {
+        try {
+            const mek = chatUpdate.messages[0];
+            if (!mek.message) return;
+
+            mek.message = (Object.keys(mek.message)[0] === "ephemeralMessage") ?
+                mek.message.ephemeralMessage.message : mek.message;
+
+            if (mek.key && mek.key.remoteJid === "status@broadcast") return;
+
+            if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
+
+            const m = smsg(client, mek, store);
+            require("./menu/case")(client, m, chatUpdate, store);
+        } catch (err) {
+            console.log(chalk.yellow.bold("[ ERROR ] case.js :\n") + chalk.redBright(util.format(err)));
+        }
+    });
+
+    client.ev.on("contacts.update", update => {
+        for (let contact of update) {
+            const id = client.decodeJid(contact.id);
+            if (store && store.contacts) {
+                store.contacts[id] = {
+                    id,
+                    name: contact.notify
+                };
+            }
+        }
     });
 
     client.decodeJid = (jid) => {
         if (!jid) return jid;
         if (/:\d+@/gi.test(jid)) {
-            let decode = jidDecode(jid) || {};
-            return decode.user && decode.server && decode.user + '@' + decode.server || jid;
-        } else return jid;
+            const decode = jidDecode(jid) || {};
+            return (decode.user && decode.server) ? decode.user + "@" + decode.server : jid;
+        }
+        return jid;
     };
 
-    client.ev.on('messages.upsert', async chatUpdate => {
-        try {
-            mek = chatUpdate.messages[0];
-            if (!mek.message) return;
-            mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message;
-            if (mek.key && mek.key.remoteJid === 'status@broadcast') return;
-            if (!client.public && !mek.key.fromMe && chatUpdate.type === 'notify') return;
-            if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return;
-            let m = smsg(client, mek, store);
-            require("./menu/case")(client, m, chatUpdate, store);
-        } catch (error) {
-            console.error("Error processing message upsert:", error);
+    client.getName = async (jid, withoutContact = false) => {
+        const id = client.decodeJid(jid);
+        const contact = store.contacts[id] || {};
+        if (id.endsWith("@g.us")) {
+            const metadata = await client.groupMetadata(id).catch(() => null);
+            return metadata?.subject || contact.name || contact.subject || id;
         }
-    });
+        return (
+            (!withoutContact ? contact.name : "") ||
+            contact.subject ||
+            contact.verifiedName ||
+            PhoneNumber("+" + id.replace("@s.whatsapp.net", "")).getNumber("international")
+        );
+    };
+    
+    client.sendText = (jid, text, quoted = '', options) => client.sendMessage(jid, { text, ...options }, { quoted });
+    
+    client.sendTextWithMentions = async (jid, text, quoted, options = {}) =>
+        client.sendMessage(jid, {
+            text,
+            contextInfo: {
+                mentionedJid: [...text.matchAll(/@(\d{0,16})/g)].map(v => v[1] + "@s.whatsapp.net"),
+            },
+            ...options,
+        }, { quoted });
 
-    client.getFile = async (PATH, save) => {
-        let res;
-        let data = Buffer.isBuffer(PATH) ? PATH : /^data:.*?\/.*?;base64,/i.test(PATH) ? Buffer.from(PATH.split`,`[1], 'base64') : /^https?:\/\//.test(PATH) ? await (res = await getBuffer(PATH)) : fs.existsSync(PATH) ? (filename = PATH, fs.readFileSync(PATH)) : typeof PATH === 'string' ? PATH : Buffer.alloc(0);
-        let type = await FileType.fromBuffer(data) || { mime: 'application/octet-stream', ext: '.bin' };
-        filename = path.join(__filename, '../' + new Date * 1 + '.' + type.ext);
-        if (data && save) fs.promises.writeFile(filename, data);
-        return { res, filename, size: await getSizeMedia(data), ...type, data };
+    client.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+        const buffer = await resolveStickerBuffer(path, options, true);
+        await client.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted });
+        return buffer;
+    };
+
+    client.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+        const buffer = await resolveStickerBuffer(path, options, false);
+        await client.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted });
+        return buffer;
+    };
+
+    async function resolveStickerBuffer(path, options, isImage = true) {
+        let buff = Buffer.isBuffer(path)
+            ? path
+            : /^data:.*?\/.*?;base64,/i.test(path)
+                ? Buffer.from(path.split(",")[1], "base64")
+                : /^https?:\/\//.test(path)
+                    ? await (await getBuffer(path))
+                    : fs.existsSync(path)
+                        ? fs.readFileSync(path)
+                        : Buffer.alloc(0);
+
+        if (options && (options.packname || options.author)) {
+            return isImage ? await writeExifImg(buff, options) : await writeExifVid(buff, options);
+        } else {
+            return isImage ? await imageToWebp(buff) : await videoToWebp(buff);
+        }
+    }
+
+    client.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+        const quoted = message.msg || message;
+        const mime = quoted.mimetype || "";
+        const messageType = message.mtype || mime.split("/")[0];
+
+        const stream = await downloadContentFromMessage(quoted, messageType);
+        let buffer = Buffer.from([]);
+
+        for await (const chunk of stream) {
+            buffer = Buffer.concat([buffer, chunk]);
+        }
+
+        const type = await FileType.fromBuffer(buffer);
+        const trueFileName = attachExtension ? `${filename}.${type.ext}` : filename;
+        fs.writeFileSync(trueFileName, buffer);
+
+        return trueFileName;
     };
 
     client.downloadMediaMessage = async (message) => {
-        let mime = (message.msg || message).mimetype || '';
-        let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0];
+        const mime = (message.msg || message).mimetype || '';
+        const messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0];
         const stream = await downloadContentFromMessage(message, messageType);
         let buffer = Buffer.from([]);
         for await (const chunk of stream) {
@@ -180,95 +260,48 @@ async function connectToWhatsApp() {
         return buffer;
     };
 
-    client.sendText = (jid, text, quoted = '', options) => client.sendMessage(jid, { text, ...options }, { quoted });
-
-    client.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
-        let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0);
-        let buffer = options && (options.packname || options.author) ? await writeExifImg(buff, options) : await imageToWebp(buff);
-        await client.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted });
-        return buffer;
-    };
-
-    client.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
-        let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0);
-        let buffer = options && (options.packname || options.author) ? await writeExifVid(buff, options) : await videoToWebp(buff);
-        await client.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted });
-        return buffer;
-    };
-
-    client.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
-        let quoted = message.msg ? message.msg : message;
-        let mime = (message.msg || message).mimetype || '';
-        let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0];
-        const stream = await downloadContentFromMessage(quoted, messageType);
-        let buffer = Buffer.from([]);
-        for await (const chunk of stream) {
-            buffer = Buffer.concat([buffer, chunk]);
-        }
-        let type = await FileType.fromBuffer(buffer);
-        let trueFileName = attachExtension ? (filename + '.' + type.ext) : filename;
-        await fs.writeFileSync(trueFileName, buffer);
-        return trueFileName;
-    };
-
-    // Tambahan fungsi send media
-    client.sendMedia = async (jid, path, caption = '', quoted = '', options = {}) => {
-        let { mime, data } = await client.getFile(path, true);
-        let messageType = mime.split('/')[0];
-        let messageContent = {};
-        
-        if (messageType === 'image') {
-            messageContent = { image: data, caption: caption, ...options };
-        } else if (messageType === 'video') {
-            messageContent = { video: data, caption: caption, ...options };
-        } else if (messageType === 'audio') {
-            messageContent = { audio: data, ptt: options.ptt || false, ...options };
-        } else {
-            messageContent = { document: data, mimetype: mime, fileName: options.fileName || 'file' };
+    client.copyNForward = async (jid, message, forceForward = false, options = {}) => {
+        if (options.readViewOnce) {
+            const vtype = Object.keys(message.message.viewOnceMessage.message)[0];
+            delete message.message.viewOnceMessage.message[vtype].viewOnce;
+            message.message = message.message.viewOnceMessage.message;
         }
 
-        await client.sendMessage(jid, messageContent, { quoted });
-    };
+        const mtype = Object.keys(message.message)[0];
+        const content = await generateForwardMessageContent(message, forceForward);
+        const objtype = Object.keys(content)[0];
+        const context = message.message[mtype]?.contextInfo || {};
 
-    client.sendPoll = async (jid, question, options) => {
-        const pollMessage = {
-            pollCreationMessage: {
-                name: question,
-                options: options.map(option => ({ optionName: option })),
-                selectableCount: 1,
-            },
+        content[objtype].contextInfo = {
+            ...context,
+            ...content[objtype].contextInfo,
         };
 
-        await client.sendMessage(jid, pollMessage);
-    };
-
-    client.setStatus = async (status) => {
-        await client.query({
-            tag: 'iq',
-            attrs: { to: '@s.whatsapp.net', type: 'set', xmlns: 'status' },
-            content: [{ tag: 'status', attrs: {}, content: Buffer.from(status, 'utf-8') }],
-        });
-        console.log(chalk.yellow(`Status updated: ${status}`));
-    };
-
-    client.public = true;
-
-    client.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update;
-        if (connection === 'close') {
-            if (lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut) {
-                connectToWhatsApp();
+        const msg = await generateWAMessageFromContent(jid, content, {
+            ...content[objtype],
+            ...options,
+            contextInfo: {
+                ...content[objtype].contextInfo,
+                ...(options.contextInfo || {})
             }
-        } else if (connection === 'open') {
-        console.log(chalk.green("success connected"))
-        client.newsletterFollow("120363321780343299@newsletter")
-        }
-    });
+        });
 
-    client.ev.on('error', (err) => {
-        console.error(chalk.red("Error: "), err.message || err);
-    });
+        await client.relayMessage(jid, msg.message, { messageId: msg.key.id });
+        return msg;
+    };
 
-    client.ev.on('creds.update', saveCreds);
+    client.serializeM = (m) => smsg(client, m, store);
+
+    client.ev.on("creds.update", saveCreds);
+
+    return client;
 }
+
 connectToWhatsApp();
+
+fs.watchFile(require.resolve(__filename), () => {
+    fs.unwatchFile(require.resolve(__filename));
+    console.log(chalk.redBright(`Update ${__filename}`));
+    delete require.cache[require.resolve(__filename)];
+    require(__filename);
+});
